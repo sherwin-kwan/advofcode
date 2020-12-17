16c7f4d43de8b85e94aed1d866784cb5481c1751
Sherwin <sherwin.kwan92@gmail.com>
2020-12-17 00:46:20 -0800

    add classes to ruby files to prevent polluting top level namespace

--- 01.rb
+++ 01.rb
@@ -1,44 +1,48 @@
-# Read file
-# Use new line separator to create an array of the numbers
-# Convert numbers from string to integer
-# Now we have the array of numbers we need to iterate through
+class Exercise1
 
-arr_str = open("input-01.txt").read.split("\n")
-my_arr = arr_str.map do |str|
-  str = str.to_i
-end
+  # Read file
+  # Use new line separator to create an array of the numbers
+  # Convert numbers from string to integer
+  # Now we have the array of numbers we need to iterate through
 
-# Base case: Find a single number in an array
-# Recursive case: with N numbers to sum. Pick first number, then find whether there's a match for the rest of the numbers
+  arr_str = open("01-input.txt").read.split("\n")
+  my_arr = arr_str.map do |str|
+    str = str.to_i
+  end
 
-def find_matches(arr, nums_to_sum, desired_sum)
-  arr = arr.sort
-  if nums_to_sum == 1
-    return (arr.index(desired_sum)) ? [desired_sum] : nil;
-  else
-    arr.each do |num|
-      potential_matches = find_matches(arr, nums_to_sum - 1, desired_sum - num)
-      if potential_matches
-        return [num].concat potential_matches
-      elsif num > (desired_sum / nums_to_sum) # If you are summing 2 numbers, a match will be found by the time you get to half of desired sum
-        return nil
+  # Base case: Find a single number in an array
+  # Recursive case: with N numbers to sum. Pick first number, then find whether there's a match for the rest of the numbers
+
+  def self.find_matches(arr, nums_to_sum, desired_sum)
+    arr = arr.sort
+    if nums_to_sum == 1
+      return (arr.index(desired_sum)) ? [desired_sum] : nil
+    else
+      arr.each do |num|
+        potential_matches = find_matches(arr, nums_to_sum - 1, desired_sum - num)
+        if potential_matches
+          return [num].concat potential_matches
+        elsif num > (desired_sum / nums_to_sum) # If you are summing 2 numbers, a match will be found by the time you get to half of desired sum
+          return nil
+        end
       end
     end
   end
-end
 
-def find_product(arr, nums_to_sum, desired_sum)
-  matches = find_matches(arr, nums_to_sum, desired_sum)
-  if matches
-    puts "The product of #{matches.join(", ")} is #{matches.reduce(:*)}"
-  else
-    puts "No solution found"
+  def self.find_product(arr, nums_to_sum, desired_sum)
+    matches = find_matches(arr, nums_to_sum, desired_sum)
+    if matches
+      puts "The product of #{matches.join(", ")} is #{matches.reduce(:*)}"
+    else
+      puts "No solution found"
+    end
   end
+
+  find_product(my_arr, 2, 2020)
+  find_product(my_arr, 3, 2020)
+
+  # Output
+  # The product of 618, 1402 is 866436
+  # The product of 545, 547, 928 is 276650720
+
 end
-
-find_product(my_arr, 2, 2020)
-find_product(my_arr, 3, 2020)
-
-# Output
-# The product of 618, 1402 is 866436
-# The product of 545, 547, 928 is 276650720
