9d39dc6a51d79766b00feb750ed319a31a155fc6
Sherwin <sherwin.kwan92@gmail.com>
2020-12-28 16:42:32 -0800

    add day 22 solution

--- 22.js
+++ 22.js
@@ -11,6 +11,8 @@
 // Add a check for repetition of position and award a win to Player 1
 
 const { getData } = require("./helpers");
+const inspect = require("util").inspect;
+const isEqual = require("lodash.isequal");
 
 const populateArrays = async (fn) => {
   const decks = await getData(fn, "\n\n", false);
@@ -22,8 +24,8 @@ const populateArrays = async (fn) => {
     .split("\n")
     .slice(1)
     .map((str) => Number(str));
-  const arr1 = new Array;
-  const arr2 = new Array;
+  const arr1 = new Array();
+  const arr2 = new Array();
   for (const card of player1) {
     arr1.push(card);
   }
@@ -37,52 +39,61 @@ const calculateWinningScore = (arr) => {
   return arr.reduce((acc, val, ind, arr) => acc + val * (arr.length - ind), 0);
 };
 
-const playGame = (arr1, arr2) => {
+const playGame = (arr1, arr2, recursive, previous, level) => {
   let i = 0;
   while (arr1.length && arr2.length) {
-    playOneRound(arr1, arr2, true);
+    playOneRound(arr1, arr2, recursive, level); // Check to make sure this isn't a repetition of position
+    for (const previousPosition of previous) {
+      if (isEqual(previousPosition, { arr1, arr2 })) {
+        console.log(`Repetition of position. Player 1 wins ${level ? 'level ' + level + ' subgame' : 'main game'} after ${i} turns by default`);
+        return "1";
+      }
+    }
+    // If it's not repetition of positoin, copy the arrays and push them into the list of previous arrays
+    previous.push({
+      arr1: [...arr1],
+      arr2: [...arr2],
+    });
     i++;
-    console.log(
-      `Turn ${i}. Player 1 has ${arr1}, Player 2 has ${arr2}`
-    );
+    if (level === 0) console.log(`Turn ${i}, Level ${level}. Player 1 has ${arr1}, Player 2 has ${arr2}`);
   }
-  const winningSet = arr1.length ? arr1 : arr2;
-  console.log(
-    `Winner's score is: ${calculateWinningScore(Array.from(winningSet))}`
-  );
-  return winningSet;
+  const winningArr = arr1.length ? 'arr1' : 'arr2';
+  if (level) {
+    console.log(`Player ${winningArr.slice(3)} wins level ${level} subgame after ${i} turns`);
+  } else {
+    console.log(`Player ${winningArr.slice(3)} wins game after ${i} turns with a score of: ${calculateWinningScore(eval(winningArr))}`);
+  };
+  return winningArr.slice(3);
 };
 
-const playOneRound = (arr1, arr2, recursive) => {
+const playOneRound = (arr1, arr2, recursive, level) => {
   const card1 = arr1.shift();
   const card2 = arr2.shift();
+  let subGameWinner = 0;
   if (recursive && card1 <= arr1.length && card2 <= arr2.length) {
-    console.log("RECURSION");
     const arr1Recursive = arr1.slice(0, card1);
     const arr2Recursive = arr2.slice(0, card2);
-    console.log(arr1Recursive);
-    console.log(arr2Recursive);
-    console.log(`Card 1 is ${card1} and card 2 is ${card2}`);
-    const subGameWinner = playGame(arr1Recursive, arr2Recursive, true);
-    console.log(`Card 1 is ${card1} and card 2 is ${card2}`);
-    if (subGameWinner === arr1Recursive) {
-      arr1.push(card1);
-      arr1.push(card2);
-    } else {
-      arr2.push(card2);
-      arr2.push(card1);
-    }
+    const prevRecursive = new Array();
+    subGameWinner = playGame(
+      arr1Recursive,
+      arr2Recursive,
+      true,
+      prevRecursive,
+      level + 1
+    );
+  }
+  if (subGameWinner === "1" || (!subGameWinner && card1 > card2)) {
+    arr1.push(card1);
+    arr1.push(card2);
+  } else if (subGameWinner === "2" || (!subGameWinner && card2 > card1)) {
+    arr2.push(card2);
+    arr2.push(card1);
   } else {
-    if (card1 > card2) {
-      arr1.push(card1);
-      arr1.push(card2);
-    } else {
-      arr2.push(card2);
-      arr2.push(card1);
-    }
+    throw new Error('Something went wrong');
   }
 };
 
-populateArrays("./22-test.txt").then((res) => {
-  playGame(res.arr1, res.arr2);
+populateArrays("./22-input.txt").then((res) => {
+  const previous = new Array();
+  playGame(res.arr1, res.arr2, true, previous, 0);
 });
