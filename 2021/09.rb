9027248267e4cd268d87d9831f936b71a8dfbe05
sherwin-kwan <sherwin.kwan92@gmail.com>
2021-12-08 22:42:19 -0800

    add day 9 solution 2021

---
+++ 2021/09.rb
@@ -0,0 +1,48 @@
+class BasinPuzzle
+  def initialize(file)
+    @data = open(file).each_line.map{_1.strip.split("").map(&:to_i)}
+    @w = @data[0].length
+    @h = @data.count
+    @basins = Array.new(@h){Array.new(@w)} # Saves which basin # a given point (r, c) is part of
+  end
+
+  def adjacent(r, c)
+    arr = []
+    arr << [r - 1, c] unless r == 0
+    arr << [r, c - 1] unless c == 0
+    arr << [r + 1, c] unless r == @h - 1
+    arr << [r, c + 1] unless c == @w - 1
+    arr
+  end
+
+  def flood_fill(r, c, basin_num)
+    adjacent(r, c).each do |x, y|
+      if !@basins[x][y] && @data[x][y] != 9
+        @basins[x][y] = basin_num
+        flood_fill(x, y, basin_num)
+      end
+    end
+  end
+
+  def solve
+    acc = 0
+    basin_count = 0
+    (0...@h).each do |r|
+      (0...@w).each do |c|
+        # Part 1 stuff
+        num = @data[r][c]
+        acc += (num + 1) if adjacent(r, c).map{|x, y| @data[x][y]}.min > num
+        # Part 2 stuff
+        next if num == 9 || @basins[r][c] # No basin here, or it's part of an existing one!
+        basin_count += 1
+        @basins[r][c] = basin_count
+        flood_fill(r, c, basin_count)
+      end
+    end
+    puts "Part 1: #{acc}"
+    puts "Part 2: #{@basins.flatten.compact.tally.sort{_2.last <=> _1.last}.first(3).map{_1.last}.reduce(&:*)}" 
+  end
+end
+
+b = BasinPuzzle.new("./09.txt")
+b.solve
